b8aeaedb23ee8bb3dede503de2122ccf215181b4
raydavis <wingeeddd2gmail.com>
2021-06-28 12:58:39 +0300

    new commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        StudentDao studentDao = new StudentDao();
+        System.out.println(studentDao.getAll());
+    }
+}
